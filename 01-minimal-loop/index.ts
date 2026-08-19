12016f21984088f9653809ca172809e86ef0828c
Johan García Sánchez <44143880+jgarciasanchez@users.noreply.github.com>
2026-08-19 15:37:26 -0600

    wip: agent loop structure, pending tool declarations

--- 01-minimal-loop/index.ts
+++ 01-minimal-loop/index.ts
@@ -1,18 +1,43 @@
 import Anthropic from "@anthropic-ai/sdk";
+import type { ContentBlock, Message, MessageParam } from "@anthropic-ai/sdk/resources";
 
 const client = new Anthropic();
 
-const msg = await client.messages.create({
-  model: "claude-sonnet-5",
-  max_tokens: 1024,
-  messages: [{
-    role: "user",
-    content: "hello, Claude"
-  }],
-});
-for (const block of msg.content) {
-  if (block.type === "text") {
-    console.log(block.text);
+// const msg: Message = await client.messages.create({
+//   model: "claude-haiku-4-5",
+//   max_tokens: 1024,
+//   messages: [{
+//     role: "user",
+//     content: "hello, Claude"
+//   }],
+// });
+// console.log(msg);
+
+// for (const block of msg.content) {
+//   if (block.type === "text") {
+//     console.log(block.text);
+//   }
+// }
+
+const runAgent = async (userMsg: string) => {
+  const messages: MessageParam[] = [{ role: "user", content: userMsg }]
+
+
+  while (true) {
+
+    const response: Message = await client.messages.create({
+      model: "claude-haiku-4-5",
+      max_tokens: 1024,
+      messages: messages,
+    });
+
+    messages.push({ role: "assistant", content: response.content })
+
+    if (response.stop_reason != 'tool_use') return response.stop_reason;
+
+
+
 
   }
 }
+
